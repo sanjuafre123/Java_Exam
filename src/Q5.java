3d121fce7c37dec239b5d6aa099c08e1871715ab
Sanju afre <148860124+sanjuafre123@users.noreply.github.com>
2024-08-24 20:35:25 +0530

    Update Q5.java

--- src/Q5.java
+++ src/Q5.java
@@ -1,17 +1,17 @@
 public class Q5 {
     public static void main(String[] args) {
-        int sum = 0;
-        int i;
-        int j;
-        for (i = 0; i <= 9; i++) {
+        int i = 7; 
+        int j = 11; 
 
-            for (j = 0; j <=9; j++)
-            {
-                if(j==1)
-                {
-                    System.out.println(" * ");
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= 11; j++) {
+                if (i == 2 && (j == 5 || j == 7) || i == 3 && (j == 4 || j == 8) || i == 4 && (j == 3 || j == 9) || i == 5 && (j == 2 || j == 10) || i == 6 && (j == 1 || j == 11) || i == 7l̥) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
                 }
             }
+            System.out.println();
         }
     }
 }
